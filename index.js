3f4af6ad6fa8fa9fb09d79bf6b78f806c54b7b4c
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-14 21:32:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a new object using the spread operator, including the original employee properties
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update the employee object destructively and return it
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object without the specified key using the spread operator
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key destructively and return the modified employee
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  
+  
+  
